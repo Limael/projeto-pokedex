0628b595c1975b0fe369d7b518f1ccc0ae35f798
Limael <limagustavorafael@hotmail.com>
2023-05-22 17:57:06 -0300

    tentando pegar tipos

--- Pokedex/src/Components/Pokedex/index.tsx
+++ Pokedex/src/Components/Pokedex/index.tsx
@@ -8,6 +8,7 @@ import {
     MenuOptionGroup,
     Button,
     SimpleGrid,
+    Box,
 
 } from '@chakra-ui/react'
 import { ChevronDownIcon } from '@chakra-ui/icons'
@@ -18,19 +19,38 @@ import api from '../../api/api'
 
 
 export const Pokedex = () => {
-    const [pokemonList, setPokemonList] = useState([]);
+    const [pokemonCount, setPokemonCount] = useState([]);
+    const [pokemonTypes, setPokemonTypes] = useState<PokemonType[]>([]);
+
+    interface PokemonType {
+        name: string;
+        url: string;
+    }
 
     useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await api.get('/pokemon');
-          setPokemonList(response.data.count);
-        } catch (error) {
-          console.log(error);
-        }
-      };
-  
-      fetchData();
+        const fetchPokemonTypes = async () => {
+            try {
+                const response = await api.get('/type');
+                setPokemonTypes(response.data.results);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchPokemonTypes();
+    }, []);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await api.get('/pokemon');
+                setPokemonCount(response.data.count);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchData();
     }, []);
     return (
         <>
@@ -38,21 +58,23 @@ export const Pokedex = () => {
             <section className={styles.section_container}>
                 <main className={styles.pokedex_container}>
                     <h1 className={styles.pokemon_quantity}>
-                    {pokemonList} <strong>Pokemons</strong> for you to choose your favorite
+                        {pokemonCount} <strong>Pokemons</strong> for you to choose your favorite
                     </h1>
 
                     <input className={styles.input} type="text" placeholder='Encontre o seu pokemon' />
 
                     <div className={styles.filter_section}>
-                        <Menu closeOnSelect={false}>
-                            <MenuButton as={Button} >
+                        <Menu closeOnSelect={false} >
+                            <MenuButton as={Button}  >
                                 MenuItem
                             </MenuButton>
                             <MenuList minWidth='240px'>
                                 <MenuOptionGroup type='checkbox'>
-                                    <MenuItemOption value='email'>Email</MenuItemOption>
-                                    <MenuItemOption value='phone'>Phone</MenuItemOption>
-                                    <MenuItemOption value='country'>Country</MenuItemOption>
+                                    {pokemonTypes.map((type) => (
+                                        <MenuItemOption key={type.name} value={type.name}>
+                                            {type.name}
+                                        </MenuItemOption>
+                                    ))}
                                 </MenuOptionGroup>
                             </MenuList>
                         </Menu>
