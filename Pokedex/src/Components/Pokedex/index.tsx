74da132fd1aca72c8c03ad4cabdc8c137eead349
Limael <limagustavorafael@hotmail.com>
2023-05-24 08:24:18 -0300

    modal abrindo e pegando informações

--- Pokedex/src/Components/Pokedex/index.tsx
+++ Pokedex/src/Components/Pokedex/index.tsx
@@ -7,249 +7,266 @@ import { CardPokemon, PokemonTypeProps } from '../CardPokemon';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 
 type PokemonType = {
-    name: string;
-    url: string;
+  name: string;
+  url: string;
 };
 
 const PAGE_SIZE = 9;
 
 export const Pokedex = () => {
-    const [pokemonCount, setPokemonCount] = useState<number>(0);
-    const [pokemonTypes, setPokemonTypes] = useState<PokemonType[]>([]);
-    const [pokemonList, setPokemonList] = useState<PokemonTypeProps[]>([]);
-    const [pokemonSearchList, setPokemonSearchList] = useState<PokemonTypeProps[]>([]);
-    const [offset, setOffset] = useState<number>(0);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [pokemonCount, setPokemonCount] = useState<number>(0);
+  const [pokemonTypes, setPokemonTypes] = useState<PokemonType[]>([]);
+  const [pokemonList, setPokemonList] = useState<PokemonTypeProps[]>([]);
+  const [pokemonSearchList, setPokemonSearchList] = useState<PokemonTypeProps[]>([]);
+  const [offset, setOffset] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
 
-    const [selectedPokemon, setSelectedPokemon] = useState<PokemonTypeProps | null>(null);
+  const [selectedPokemon, setSelectedPokemon] = useState<PokemonTypeProps | null>(null);
 
 
 
-    const [searchValue, setSearchValue] = useState('')
-    useEffect(() => {
-        const fetchPokemonList = async () => {
-            if (searchValue.trim() === '') {
-                setPokemonSearchList([])
-                return;
-            }
+  const [searchValue, setSearchValue] = useState('')
+  useEffect(() => {
+    const fetchPokemonList = async () => {
+      if (searchValue.trim() === '') {
+        setPokemonSearchList([])
+        return;
+      }
 
-            setLoading(true);
+      setLoading(true);
 
-            try {
-                const response = await api.get(`/pokemon/${searchValue}`);
-                const data = response.data;
+      try {
+        const response = await api.get(`/pokemon/${searchValue}`);
+        const data = response.data;
 
-                const pokemon: PokemonTypeProps = {
-                    name: data.name,
-                    attack: data.stats[1].base_stat,
-                    defense: data.stats[2].base_stat,
-                    types: data.types.map((type: { type: { name: string } } & PokemonType) => type.type.name),
-                    image: data.sprites.other['official-artwork'].front_default,
-                    onClick: () => handlePokemonClick(pokemon), // <- Passe o Pokémon como argumento
-                };
+        const pokemon: PokemonTypeProps = {
+          name: data.name,
+          attack: data.stats[1].base_stat,
+          defense: data.stats[2].base_stat,
+          types: data.types.map((type: { type: { name: string } } & PokemonType) => type.type.name),
+          image: data.sprites.other['official-artwork'].front_default,
+          onClick: () => handlePokemonClick(pokemon),
+          hp: data.stats[0].base_stat,
+          spAttack: data.stats[3].base_stat,
+          spDefense: data.stats[4].base_stat,
+          experience: data.base_experience,
+          generation: data.game_indices[0].version.url.split('/').slice(-2, -1)[0],
+          index: data.id,
 
-
-                setPokemonSearchList([pokemon]);
-                setLoading(false);
-            } catch (error) {
-                console.error('Error fetching Pokemon:', error);
-                setLoading(false);
-            }
         };
 
-        fetchPokemonList();
-    }, [searchValue]);
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchValue(event.target.value);
+        setPokemonSearchList([pokemon]);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching Pokemon:', error);
+        setLoading(false);
+      }
     };
 
+    fetchPokemonList();
+  }, [searchValue]);
 
-    /*     const handleTypeSelect = (type: string) => {
-            setSelectedTypes((prevSelectedTypes) => {
-                if (prevSelectedTypes.includes(type)) {
-                    return prevSelectedTypes.filter((selectedType) => selectedType !== type);
-                } else {
-                    return [...prevSelectedTypes, type];
-                }
-            });
-        };
-    
-        useEffect(() => {
-            const lastSelectedType = selectedTypes[selectedTypes.length - 1];
-            console.log('Último tipo selecionado:', lastSelectedType);
-        }, [selectedTypes]);
-    
-     */
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
 
-    useEffect(() => {
-        const fetchPokemonTypes = async () => {
-            try {
-                const response = await api.get('/type');
-                setPokemonTypes(response.data.results);
-            } catch (error) {
-                console.log(error);
-            }
-        };
 
-        fetchPokemonTypes();
-    }, []);
+  /*     const handleTypeSelect = (type: string) => {
+          setSelectedTypes((prevSelectedTypes) => {
+              if (prevSelectedTypes.includes(type)) {
+                  return prevSelectedTypes.filter((selectedType) => selectedType !== type);
+              } else {
+                  return [...prevSelectedTypes, type];
+              }
+          });
+      };
+  
+      useEffect(() => {
+          const lastSelectedType = selectedTypes[selectedTypes.length - 1];
+          console.log('Último tipo selecionado:', lastSelectedType);
+      }, [selectedTypes]);
+  
+   */
 
-    /* 
-        useEffect(() => {
-            const fetchPokemonTypeName = async () => {
-                try {
-                    const lastSelectedType = selectedTypes[selectedTypes.length - 1];
-                    const response = await api.get(`/type/${lastSelectedType}`);
-                    setFilteredList(response.data.pokemon);
-                } catch (error) {
-                    console.log(error);
-                }
+  useEffect(() => {
+    const fetchPokemonTypes = async () => {
+      try {
+        const response = await api.get('/type');
+        setPokemonTypes(response.data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchPokemonTypes();
+  }, []);
+
+  /* 
+      useEffect(() => {
+          const fetchPokemonTypeName = async () => {
+              try {
+                  const lastSelectedType = selectedTypes[selectedTypes.length - 1];
+                  const response = await api.get(`/type/${lastSelectedType}`);
+                  setFilteredList(response.data.pokemon);
+              } catch (error) {
+                  console.log(error);
+              }
+          };
+  
+          fetchPokemonTypeName();
+      }, [selectedTypes]); */
+
+
+
+
+
+
+
+  useEffect(() => {
+    const fetchPokemonList = async () => {
+      setLoading(true);
+
+      try {
+        const response = await api.get(`/pokemon?limit=${PAGE_SIZE}&offset=${offset}`);
+        setPokemonCount(response.data.count);
+
+        const results: PokemonType[] = response.data.results;
+
+        const formattedPokemonList: PokemonTypeProps[] = await Promise.all(
+          results.map(async (pokemon: PokemonType) => {
+            const response = await api.get(`/pokemon/${pokemon.name}`);
+            const data = response.data;
+            const formattedPokemon: PokemonTypeProps = {
+              name: data.name,
+              attack: data.stats[1].base_stat,
+              defense: data.stats[2].base_stat,
+              types: data.types.map((type: { type: { name: string } } & PokemonType) => type.type.name),
+              image: data.sprites.other['official-artwork'].front_default,
+              onClick: () => handlePokemonClick(formattedPokemon),
+              hp: data.stats[0].base_stat,
+              spAttack: data.stats[3].base_stat,
+              spDefense: data.stats[4].base_stat,
+              experience: data.base_experience,
+              generation: data.game_indices[0].version.url.split('/').slice(-2, -1)[0],
+              index: data.id,
+
             };
-    
-            fetchPokemonTypeName();
-        }, [selectedTypes]); */
+            return formattedPokemon;
+          })
+        );
 
+        if (offset === 0) {
+          setPokemonList(formattedPokemonList);
+        } else {
+          setPokemonList((prevList) => [...prevList, ...formattedPokemonList]);
+        }
 
-
-
-
-
-
-    useEffect(() => {
-        const fetchPokemonList = async () => {
-            setLoading(true);
-
-            try {
-                const response = await api.get(`/pokemon?limit=${PAGE_SIZE}&offset=${offset}`);
-                setPokemonCount(response.data.count);
-
-                const results: PokemonType[] = response.data.results;
-
-                const formattedPokemonList: PokemonTypeProps[] = await Promise.all(
-                    results.map(async (pokemon: PokemonType) => {
-                        const response = await api.get(`/pokemon/${pokemon.name}`);
-                        const data = response.data;
-                        const formattedPokemon: PokemonTypeProps = {
-                            name: data.name,
-                            attack: data.stats[1].base_stat,
-                            defense: data.stats[2].base_stat,
-                            types: data.types.map((type: { type: { name: string } } & PokemonType) => type.type.name),
-                            image: data.sprites.other['official-artwork'].front_default,
-                            onClick: () => handlePokemonClick(formattedPokemon), // Correção: Passe o Pokémon formatado como argumento
-                        };
-                        return formattedPokemon;
-                    })
-                );
-
-                if (offset === 0) {
-                    setPokemonList(formattedPokemonList);
-                } else {
-                    setPokemonList((prevList) => [...prevList, ...formattedPokemonList]);
-                }
-
-                setLoading(false);
-            } catch (error) {
-                console.error('Error fetching Pokemon list:', error);
-            }
-        };
-
-        fetchPokemonList();
-    }, [offset]);
-
-
-    const handleLoadMore = () => {
-        setOffset((prevOffset) => prevOffset + PAGE_SIZE);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching Pokemon list:', error);
+      }
     };
 
-    const handlePokemonClick = (pokemon: PokemonTypeProps) => {
-        setSelectedPokemon(pokemon);
-        setIsModalOpen(true);
-    };
-
-    const handleCloseModal = () => {
-
-        setIsModalOpen(false);
-    };
-
-    return (
-        <>
-            <Header />
-            <section className={styles.section_container}>
-                <main className={styles.pokedex_container}>
-                    <h1 className={styles.pokemon_quantity}>
-                        {pokemonCount} <strong>Pokemons</strong> for you to choose your favorite
-                    </h1>
-
-                    <input className={styles.input} type="text" value={searchValue} onChange={handleInputChange} placeholder="Encontre o seu pokemon" />
-
-                    <div className={styles.filter_section}>
-                        <Menu closeOnSelect={false}>
-                            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                                Tipo
-                            </MenuButton>
-                            <MenuList maxHeight="15rem" overflowY="scroll">
-                                {pokemonTypes.map((type) => (
-                                    <MenuItem key={type.name}>
-                                        <Checkbox
-                                        /*                 isChecked={selectedTypes.includes(type.name)}
-                                                        onChange={() => handleTypeSelect(type.name)} */
-                                        >
-                                            {type.name}
-                                        </Checkbox>
-                                    </MenuItem>
-                                ))}
-                            </MenuList>
-                        </Menu>
-                    </div>
-
-                    <SimpleGrid columns={[1, null, 2, 3]} spacing={[0, null, '34px']}>
-                        {searchValue === '' ? (
-                            pokemonList.map((pokemon: PokemonTypeProps, index: number) => (
-                                <CardPokemon key={index} {...pokemon} onClick={() => handlePokemonClick(pokemon)} />
-                            ))
-                        ) : (
-                            pokemonSearchList.map((pokemon: PokemonTypeProps, index: number) => (
-                                <CardPokemon key={index} {...pokemon} onClick={() => handlePokemonClick(pokemon)} />
-                            ))
-                        )}
-                    </SimpleGrid>
-
-                    {searchValue !== '' ? (<></>) : !loading && pokemonList.length < pokemonCount && (
-                        <Button onClick={handleLoadMore} my="4" >
-                            Load More
-                        </Button>
-                    )}
-
-                    {loading && <CircularProgress mt="4" isIndeterminate color='yellow.300' />}
+    fetchPokemonList();
+  }, [offset]);
 
 
-                </main>
-            </section>
+  const handleLoadMore = () => {
+    setOffset((prevOffset) => prevOffset + PAGE_SIZE);
+  };
 
-            <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Modal Title</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        {selectedPokemon && (
-                            <>
-                                <h2>{selectedPokemon.name}</h2>
-                                <p>Attack: {selectedPokemon.attack}</p>
-                                <p>Defense: {selectedPokemon.defense}</p>
-                                <p>Types: {selectedPokemon.types.join(', ')}</p>
-                                <img src={selectedPokemon.image} alt={selectedPokemon.name} />
-                            </>
-                        )}
-                    </ModalBody>
+  const handlePokemonClick = (pokemon: PokemonTypeProps) => {
+    setSelectedPokemon(pokemon);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <Header />
+      <section className={styles.section_container}>
+        <main className={styles.pokedex_container}>
+          <h1 className={styles.pokemon_quantity}>
+            {pokemonCount} <strong>Pokemons</strong> for you to choose your favorite
+          </h1>
+
+          <input className={styles.input} type="text" value={searchValue} onChange={handleInputChange} placeholder="Encontre o seu pokemon" />
+
+          <div className={styles.filter_section}>
+            <Menu closeOnSelect={false}>
+              <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                Tipo
+              </MenuButton>
+              <MenuList maxHeight="15rem" overflowY="scroll">
+                {pokemonTypes.map((type) => (
+                  <MenuItem key={type.name}>
+                    <Checkbox
+                    /*                 isChecked={selectedTypes.includes(type.name)}
+                                    onChange={() => handleTypeSelect(type.name)} */
+                    >
+                      {type.name}
+                    </Checkbox>
+                  </MenuItem>
+                ))}
+              </MenuList>
+            </Menu>
+          </div>
+
+          <SimpleGrid columns={[1, null, 2, 3]} spacing={[0, null, '34px']}>
+            {searchValue === '' ? (
+              pokemonList.map((pokemon: PokemonTypeProps, index: number) => (
+                <CardPokemon key={index} {...pokemon} onClick={() => handlePokemonClick(pokemon)} />
+              ))
+            ) : (
+              pokemonSearchList.map((pokemon: PokemonTypeProps, index: number) => (
+                <CardPokemon key={index} {...pokemon} onClick={() => handlePokemonClick(pokemon)} />
+              ))
+            )}
+          </SimpleGrid>
+
+          {searchValue !== '' ? (<></>) : !loading && pokemonList.length < pokemonCount && (
+            <Button onClick={handleLoadMore} my="4" >
+              Load More
+            </Button>
+          )}
+
+          {loading && <CircularProgress mt="4" isIndeterminate color='yellow.300' />}
 
 
-                </ModalContent>
-            </Modal>
+        </main>
+      </section>
 
-        </>
-    );
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {selectedPokemon && (
+              <>
+                <strong>{selectedPokemon.index}</strong>
+                <h2>{selectedPokemon.name}</h2>
+                <p>Attack: {selectedPokemon.attack}</p>
+                <p>Defense: {selectedPokemon.defense}</p>
+                <p>SpAttack: {selectedPokemon.spAttack}</p>
+                <p>SpDefense: {selectedPokemon.spDefense}</p>
+                <p>Types: {selectedPokemon.types.join(', ')}</p>
+                <img src={selectedPokemon.image} alt={selectedPokemon.name} />
+              </>
+            )}
+          </ModalBody>
+
+
+        </ModalContent>
+      </Modal>
+
+    </>
+  );
 };
