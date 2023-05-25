b2998dd1c1420942d595ee3c12bb2993170dd2db
Gustavo Lima <limagustavorafael@hotmail.com>
2023-05-25 00:14:13 -0300

    rotas feitas

--- Pokedex/src/main.tsx
+++ Pokedex/src/main.tsx
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './global.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import { Pokedex } from './Components/Pokedex/index.js'
+import { Banner } from './Components/Banner/index.js'
+import { Legendaries } from './Components/Legendaries/index.js'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Banner />,
+  },
+  {
+    path: "pokedex",
+    element: <Pokedex />,
+  },
+  {
+    path: "legendaries",
+    element: <Legendaries />,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>,
 )
